b6a30afd6c948dcf63aad917a1ec980e128aba64
Sungbeo <dtc245010039@ictu.edu.vn>
2026-04-04 09:43:48 +0700

    đổi nền thành màu trắng

--- WMSPro_fix/obj/Debug/net8.0/WMSPro.AssemblyInfo.cs
+++ WMSPro_fix/obj/Debug/net8.0/WMSPro.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WMSPro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+217d58eab931f7ba0c91d5081c070cb542a246fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("WMSPro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WMSPro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
